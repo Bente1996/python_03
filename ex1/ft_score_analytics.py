c5ccd7deff1904d6161cbecd246b18c13140c284
Bente De Koning <bentedekoning@gmail.com>
2026-09-10 18:23:02 +0200

    bezig met ex1

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -8,33 +8,22 @@ class NoArgumentsError(Exception):
 class NonNumericError(Exception):
     pass
 
-def non_numeric(scores: list) -> bool:
-    args = len(scores)
+def is_number(scores: list) -> bool: # mee bezig
     i = 1
-    for _ in range(args - 1):
-        j = 0
-        print(f"Index {i}: {scores[i]}")
+    while i < len(scores):
+        try:
+            float(scores[i])
+        except ValueError:
+            return False
         i += 1
-        str(scores[i])
-        while scores[i][j]:
-            if scores[i][j] < '0' or scores [i][j] > '9':
-                print(scores[i])
     return True
-    #args = len(scores)
-    #i = 1
-    #while i < args:
-    #    j = 0
-    #    while scores[i][j]:
-    #        if scores[i][j] < 0 or scores[i][j] > 9:
-    #            return True
-    #    i += 1
-    #return False
 
 def check_errors(scores: list) -> None:
     if len(scores) < 2:
         raise NoArgumentsError("No arguments provided")
-    if non_numeric(scores) is True:
-        raise NonNumericError("Non numeric found")
+    #if is_number(scores) is False:
+    #    raise NonNumericError("Non numeric found")
+    is_number(scores)
 
 
 def scores_processed(scores: list) -> None:
@@ -54,5 +43,4 @@ def test_score_analytics(scores: list) -> None:
 
 if __name__ == "__main__":
     print("=== Player Score Analytics ===")
-    #test_score_analytics(sys.argv)
-    non_numeric(sys.argv)
+    test_score_analytics(sys.argv)
